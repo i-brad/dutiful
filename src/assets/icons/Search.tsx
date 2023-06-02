abb0c68fb22805aeb7f2e22405206cadea64d382
i-brad <klausmickelson52@gmail.com>
2023-06-02 22:54:51 +0100

    fix: font cors error
    - added search icon component
    - fixed navbar link

---
+++ src/assets/icons/Search.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function SearchIcon() {
+  return (
+    <svg
+      width="34"
+      height="34"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="19.6108"
+        cy="19.611"
+        r="14.9809"
+        stroke="#603F8B"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M30.0303 30.8086L35.9037 36.6667"
+        stroke="#603F8B"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default SearchIcon;
